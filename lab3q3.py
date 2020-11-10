5e707e99a4775df49623f2f5a7632808e64f53bc
peonyau <piano2friends@gmail.com>
2020-11-10 13:52:47 +0800

    Add files via upload

---
+++ lab3q3.py
@@ -0,0 +1,18 @@
+'''
+filename: lab3q3.py
+description: lab3q3
+date: nov 6 2020
+author: Au Tsz Yui
+'''
+import math
+
+# x**2 - 1 = 0
+a = 1
+b = 0
+c = -1
+
+x1 = (-b + math.sqrt(b*b- 4*a*c)) / (2*a) ,
+
+x2 = (-b - math.sqrt(b*b- 4*a*c)) / (2*a)
+
+print(x1,x2)
