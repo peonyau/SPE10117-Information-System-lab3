027786e70be950691976aa788ead42877bb01a2d
peonyau <piano2friends@gmail.com>
2020-11-10 14:19:40 +0800

    Update 2.0  lab3q3.py

--- 2.0  lab3q3.py
+++ 2.0  lab3q3.py
@@ -1,7 +1,7 @@
 '''
 filename: lab3q3.py
 description: lab3q3
-date: nov 6 2020
+date: nov 6, 2020
 author: Au Tsz Yui
 '''
 import math
@@ -11,8 +11,9 @@ a = float(input('enter a:'))
 b = float(input('enter b:'))
 c = float(input('enter c:'))
 
-x1 = (-b + math.sqrt(b*b- 4*a*c)) / (2*a) ,
+x1 = (-b + math.sqrt(b*b- 4*a*c)) / (2*a) 
 
 x2 = (-b - math.sqrt(b*b- 4*a*c)) / (2*a)
 
 print(x1,x2)
+print('%.5f.10f'%(x1, x2))
